0650034b66d49037227611ce5d36d219407d7f1e
Taejin Hwang <greathtj@gmail.com>
2023-02-14 17:17:41 +0900

    Update Vision.py

--- Vision.py
+++ Vision.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from datetime import datetime
+import cv2
 
 st.set_page_config(
     page_title="Vision Inspec.",
@@ -8,3 +10,22 @@ st.set_page_config(
 st.title("Vision Inspection 🎥")
 
 st.write("test")
+
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+isCapture = False
+
+st.write("OKOK")
+
+if st.button("촬영"):
+    isCapture = True
+
+while True:
+    ret, frame = camera.read()
+    if ret:
+        if isCapture:
+            fn = "photos/" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".jpg"
+            cv2.imwrite(fn, frame)
+            isCapture = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
