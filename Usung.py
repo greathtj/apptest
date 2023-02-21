13773bd64eba87449d577ca51cc1dd6f11d5bf02
Taejin Hwang <greathtj@gmail.com>
2023-02-21 11:25:56 +0900

    test

--- Usung.py
+++ Usung.py
@@ -3,6 +3,11 @@ import datetime
 import pandas as pd
 import customLib as cl
 
+st.set_page_config(
+    page_title="유성레이저 IoT",
+    page_icon="☄️",
+)
+
 st.title("유성레이저 설비 관리 V1.0")
 
 st.header("Dashboard")
