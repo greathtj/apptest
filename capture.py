273eb6f0757e86860c6d6d6a76ad846c4384c34b
Taejin Hwang <greathtj@gmail.com>
2023-02-22 08:59:04 +0900

    test

--- capture.py
+++ capture.py
@@ -1,32 +1,65 @@
 import streamlit as st
-import numpy as np
+import pandas as pd
 import cv2
-import mss
-import mss.tools
+from PIL import Image, ImageEnhance
+import numpy as np
+import os
+#import tensorflow as tf
+#import tensorflow_hub as hub
+import time ,sys
+from streamlit_embedcode import github_gist
+import urllib.request
+import urllib
+import moviepy.editor as moviepy
+import cv2
+import numpy as np
+import time
+import sys
 
 def main():
-    st.title("Webcam Capture")
+    new_title = '<p style="font-size: 42px;">Welcome to my Object Detection App!</p>'
+    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
 
-    # Define the screen size and capture region
-    monitor = {"top": 0, "left": 0, "width": 640, "height": 480}
+    read_me = st.markdown("""
+    This project was built using Streamlit and OpenCV 
+    to demonstrate YOLO Object detection in both videos(pre-recorded)
+    and images.
     
-    # Create a placeholder to display the captured image
-    image_placeholder = st.empty()
+    
+    This YOLO object Detection project can detect 80 objects(i.e classes)
+    in either a video or image. The full list of the classes can be found 
+    [here](https://github.com/KaranJagtiani/YOLO-Coco-Dataset-Custom-Classes-Extractor/blob/main/classes.txt)"""
+    )
+    st.sidebar.title("Select Activity")
+    choice  = st.sidebar.selectbox("MODE",("About","Object Detection(Image)","Object Detection(Video)"))
+    #["Show Instruction","Landmark identification","Show the #source code", "About"]
+    
+    if choice == "Object Detection(Image)":
+        #st.subheader("Object Detection")
+        read_me_0.empty()
+        read_me.empty()
+        #st.title('Object Detection')
+        object_detection_image()
+    elif choice == "Object Detection(Video)":
+        read_me_0.empty()
+        read_me.empty()
+        #object_detection_video.has_beenCalled = False
+        object_detection_video()
+        #if object_detection_video.has_beenCalled:
+        try:
 
-    # Add a button to capture an image
-    if st.button("Capture"):
-        # Create a screenshot of the capture region
-        with mss.mss() as sct:
-            sct_img = sct.grab(monitor)
-            # Convert the screenshot to a numpy array
-            img = np.array(sct_img)
-            # Convert the image to RGB format
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-            # Display the captured image
-            image_placeholder.image(img, caption="Captured Image")
+            clip = moviepy.VideoFileClip('detected_video.mp4')
+            clip.write_videofile("myvideo.mp4")
+            st_video = open('myvideo.mp4','rb')
+            video_bytes = st_video.read()
+            st.video(video_bytes)
+            st.write("Detected Video") 
+        except OSError:
+            ''
 
-    # Release the camera and close the window
-    cv2.destroyAllWindows()
+    elif choice == "About":
+        print()
+        
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+		main()	
